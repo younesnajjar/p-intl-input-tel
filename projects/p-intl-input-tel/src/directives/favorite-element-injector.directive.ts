9599efe6d3ddcce62b9d51ba17ba84a70717996d
Chaotic Engineer <younes.najjar.96@gmail.com>
2025-03-27 09:17:56 +0000

    Revert "Replace dropdown with the select component due PrimeNG breaking change"
    This reverts commit d6e1fa2c36ff512b742b2c0cb7cfb3802b06f936.

--- projects/p-intl-input-tel/src/directives/favorite-element-injector.directive.ts
+++ projects/p-intl-input-tel/src/directives/favorite-element-injector.directive.ts
@@ -1,19 +1,19 @@
-import { Directive, ElementRef, inject, input, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, OnInit } from '@angular/core';
 
 @Directive({
     selector: '[favorite]',
     standalone: true
 })
 export class FavoriteElementInjectorDirective implements OnInit {
-    private el = inject(ElementRef);
-    private renderer = inject(Renderer2);
+    private el;
 
-    favoriteItemsCounter = input<number>();
+    constructor(el: ElementRef) {
+        this.el = el;
+    }
 
     ngOnInit() {
-        const allFavorites = document.querySelectorAll('.favorite');
-        if (allFavorites[allFavorites.length - 1] === this.el.nativeElement && allFavorites.length === this.favoriteItemsCounter()) {
-            this.renderer.addClass(this.el.nativeElement, 'last-favorite-option');
+        if (this.el.nativeElement.classList.contains('favorite')) {
+            this.el.nativeElement.parentNode.parentNode.classList.add('favoriteItem')
         }
     }
 }
